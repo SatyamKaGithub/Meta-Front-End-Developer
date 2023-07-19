59925ab8a5b3bca349e0a8afe674b8eafbea1507
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-07-19 13:36:51 +0530

    Create App.js

---
+++ Course 5 - React Basics/Week 1 - React Components/1. Demos/2. The React Project Structure/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>I come from createElement!</h1>
+      <h2>I also come from createElement!</h2>
+    </div>
+  );
+}
+
+export default App;
