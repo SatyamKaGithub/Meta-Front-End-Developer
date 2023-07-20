2f1b9b7ab29af3480dc165441be3f3ae98fb73f2
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-07-20 21:16:40 +0530

    Add files via upload

---
+++ Course 5 - React Basics/Week 1 - React Components/1. Demos/4. Using props in Component/src/components/Main.js
@@ -0,0 +1,5 @@
+function Main(props) {
+    return <h2 className="main-component">{props.greet} from Main</h2>;
+}
+
+export default Main;
---
+++ Course 5 - React Basics/Week 1 - React Components/1. Demos/4. Using props in Component/src/components/Sidebar.js
@@ -0,0 +1,5 @@
+function Sidebar(props) {
+    return <h2 className="sidebar-component">{props.greet} from Sidebar</h2>;
+}
+
+export default Sidebar;
