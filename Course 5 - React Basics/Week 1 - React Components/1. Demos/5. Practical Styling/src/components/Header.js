0fe77b67187fe5c90f6882fe1a9aad97414bdb03
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-07-20 21:23:24 +0530

    Create Header.js

---
+++ Course 5 - React Basics/Week 1 - React Components/1. Demos/5. Practical Styling/src/components/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    const headerStyle = {
+        border: "1px solid lightgray",
+        borderRadius: "5px",
+        padding: "20px"
+    };
+    
+    return (
+        <header style={headerStyle}>
+            <h1>Header content here</h1>
+        </header>
+    );
+}
+
+export default Header;
