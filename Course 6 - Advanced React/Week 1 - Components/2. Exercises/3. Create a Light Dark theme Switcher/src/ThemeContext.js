9a886b443330b7d7fe4100f19bbcb5f60e5067d9
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-08-01 23:44:39 +0530

    Add files via upload

---
+++ Course 6 - Advanced React/Week 1 - Components/2. Exercises/3. Create a Light Dark theme Switcher/src/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { useContext, useState } from "react";
+
+const ThemeContext = React.createContext(undefined);
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light");
+
+    return (
+        <ThemeContext.Provider
+            value={{
+                theme,
+                toggleTheme: () => setTheme(theme === "light" ? "dark" : "light")
+            }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
