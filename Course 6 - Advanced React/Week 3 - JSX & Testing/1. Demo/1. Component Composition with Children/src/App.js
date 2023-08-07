04e04e5ba3b2e31deeff2957a04d8c6eea2f1af0
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-08-07 22:34:44 +0530

    Add files via upload

---
+++ Course 6 - Advanced React/Week 3 - JSX & Testing/1. Demo/1. Component Composition with Children/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+const Button = ({ children, backgroundColor }) => {
+  return <button style={{ backgroundColor }}>{children}</button>;
+};
+
+const Alert = ({ children }) => {
+  return (
+    <>
+      <div className="Overlay" />
+      <div className="Alert">{children}</div>
+    </>
+  );
+};
+
+const DeleteButton = () => {
+  return <Button backgroundColor="red">Delete</Button>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header>Little Lemon Restaurant 🍕</header>
+      <Alert>
+        <h4>Delete Account</h4>
+        <p>
+          Are you sure you want to proceed? You will miss all your delicious 
+          recipes!
+        </p>
+        <DeleteButton />
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
