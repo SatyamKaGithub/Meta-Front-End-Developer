cd013e27f6525fdbc69574104c95671c13cb4c8e
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-08-07 22:48:54 +0530

    Add files via upload

---
+++ Course 6 - Advanced React/Week 3 - JSX & Testing/1. Demo/4. Create a hoc for cursor position/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import {useState, useEffect} from 'react';
+
+// HOC---
+/**
+ * Note that "with" in the HOC's name is a general convention recommended by React
+ * since it expresses the enhancing nature of the technique, 
+ * like providing a component with something else
+ */
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+
+    const [mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0,
+    })
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      };
+
+      window.addEventListener("mousemove", handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMousePositionChange);
+      };
+    }, []);
+
+    return <WrappedComponent {...props} mousePosition={mousePosition} />
+  };
+};
+// ---HOC
+
+// Components without HOC---
+const PanelMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+  );
+}
+// ---Components without HOC
+
+// Components with HOC---
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
+// ---Components with HOC
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
+    </div>
+  );
+}
+
+export default App;
